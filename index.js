8dfd2f0dc547b65fe36d8ea5c208d7e137838555
mr-kwansa <albertkwansa0277@gmail.com>
2023-11-07 23:11:32 +0000

    added the button and its functionality all that is left with is to style the page better

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const url = 'https://api.quotable.io/random';
 var qoute;
 var author;
+var qoutetext =document.querySelector('.qoute'); 
+var authurtext =document.querySelector('.authur');
 async function fetchData(url) {
     const data= await fetch(url);
     const advicedata = await data.json();
      qoute=  advicedata.content;
      author= advicedata.author;
-    console.log(advicedata);  
+    // console.log(advicedata);  
     console.log(qoute);
     console.log(author);
-  }
+    
+    if(qoutetext && authurtext){
+        qoutetext.innerHTML= advicedata.content;
+        authurtext.innerHTML= advicedata.author;
+    }
+}
   fetchData(url);
+
+
 // var qoutetopage=document.getElementsByClassName("qoute").innerHTML;
 // var authurpage=document.getElementsByClassName("authur").innerHTML;
 // qoutetopage.innerHTML=qoute;
 // authurpage.innerHTML=author;
-function updatePage() {
-    const quoteElement = document.querySelector('quote');
-    const authorElement = document.querySelector('author');
+// function updatePage() {
+//     const quoteElement = document.querySelector('quote');
+//     const authorElement = document.querySelector('author');
   
-    quoteElement.innerHTML = qoute;
-    authorElement.innerHTML = author;
-  }
-  updatePage();
+//     quoteElement.innerHTML = qoute;
+//     authorElement.innerHTML = author;
+//   }
+//   updatePage();

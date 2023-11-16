045e2f54d787338bb0fbf551ebab8b7c62a878ec
Richard McAdams <93725119+mcthekreator@users.noreply.github.com>
2023-11-16 18:59:10 +0000

    it's working now

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const url = 'https://api.quotable.io/random';
-var qoute;
-var author;
-var qoutetext =document.getElementById("qoute");
-var authurtext =document.getElementById("authur");
+
+let qoutetext =document.getElementById("qoute");
+let authurtext =document.getElementById("authur");
+
+
+
 async function fetchData(url) {
     const data= await fetch(url);
     const advicedata = await data.json();
-     qoute=  advicedata.content;
-     author= advicedata.author;
-    // console.log(advicedata);  
-    console.log(qoute);
-    console.log(author);
+    let qoute=  advicedata.content;
+    let author= advicedata.author;
+
+     qoutetext.innerHTML = qoute;
+     authurtext.innerHTML = author;
+
+     
+// //     // console.log(advicedata);  
+// //     console.log(qoute);
+// //     console.log(author);
+// //     console.log(qoutetext)
+
+
+// YOU DIDN'T NEED AN IF STATEMENT TO IMPLEMENT IT
     
-    if(qoutetext && authurtext){
-        qoutetext.innerHTML= advicedata.content;
-        authurtext.innerHTML= advicedata.author;
-    }
+// //     if(qoutetext && authurtext){
+// //         qoutetext.innerHTML= advicedata.content;
+// //         authurtext.innerHTML= advicedata.author;
+// //     }
+// // }
+// //   fetchData(url);
+
 }
-  fetchData(url);
+
+
+
+
+// Another way to also do it
+// but when running this should take away the click funtionality in the html
+
+// document.getElementById("button").addEventListener('click', () =>
+//     fetch('https://api.quotable.io/random')
+//         .then(response => response.json())
+//         .then(random => qoutetext.innerHTML = `${random.content}`)
+// )
